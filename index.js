d2d22a341034e81851272d4f0f9b238e08dd5ccf
Sadeve Methmika <sadevemdev@gmail.com>
2024-10-21 21:33:09 +0530

    Added task completing and removing functionality, also tasks are saved in local storage.

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const todoList = document.querySelector(".todo-list");
 const button = document.querySelector(".todo-add");
 const inputBox = document.querySelector("#task-input-box")
 
+loadData();
+
 function addTask() {
     if ((inputBox.value.replace(/\s/g,'') == "")){
         alert("Input a task")
-        inputBox.value = "";
     } else {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
@@ -14,6 +15,26 @@ function addTask() {
 
         li.appendChild(span);
         todoList.appendChild(li);
+        saveData();
     }
+    inputBox.value = "";
+}
+
+todoList.addEventListener("click", (e) => {
+    if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    } else if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+}) 
+
+function saveData() {
+    localStorage.setItem("data", todoList.innerHTML);
+}
+
+function loadData() {
+    todoList.innerHTML = localStorage.getItem("data");
 }
 
